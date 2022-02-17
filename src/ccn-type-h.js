85244a1032ff9d954187920b72bfe48158cdb071
jurgenizer <jurgen.geitner@gmail.com>
2022-02-17 14:08:49 +0200

    Code simplified

--- src/ccn-type-h.js
+++ src/ccn-type-h.js
@@ -1,5 +1,5 @@
 /*
-    Cookie Consent Notice Type a
+    Cookie Consent Notice Type h
     Adapted from https://github.com/manucaralmo/GlowCookies
 */
 
@@ -32,7 +32,7 @@ class CookieConsentNotice {
   createDOMElements() {
     // COOKIES BUTTON
     this.PreBanner = document.createElement("div");
-    this.PreBanner.innerHTML = `<button type="button" id="prebannerBtn" class="prebanner prebanner__border__${this.config.bannerStyle} cookieConsentNotice__${this.config.position} cookieConsentNotice__${this.config.border} animation" style="color: ${this.banner.manageCookies.color}; background-color: ${this.banner.manageCookies.background};">
+    this.PreBanner.innerHTML = `<button type="button" id="prebannerBtn" class="prebanner cookieConsentNotice__${this.config.position} cookieConsentNotice__${this.config.border} animation" style="color: ${this.banner.manageCookies.color}; background-color: ${this.banner.manageCookies.background};">
                                       <svg fill="currentColor" style="margin-right: 7px; margin-top: 2px; vertical-align: text-top;" height="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                           <path d="M510.52 255.82c-69.97-.85-126.47-57.69-126.47-127.86-70.17 0-127-56.49-127.86-126.45-27.26-4.14-55.13.3-79.72 12.82l-69.13 35.22a132.221 132.221 0 0 0-57.79 57.81l-35.1 68.88a132.645 132.645 0 0 0-12.82 80.95l12.08 76.27a132.521 132.521 0 0 0 37.16 72.96l54.77 54.76a132.036 132.036 0 0 0 72.71 37.06l76.71 12.15c27.51 4.36 55.7-.11 80.53-12.76l69.13-35.21a132.273 132.273 0 0 0 57.79-57.81l35.1-68.88c12.56-24.64 17.01-52.58 12.91-79.91zM176 368c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm32-160c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm160 128c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"/>
                                       </svg>${this.banner.manageCookies.text}</button>`;
@@ -43,7 +43,7 @@ class CookieConsentNotice {
     this.Cookies = document.createElement("div");
     this.Cookies.innerHTML = `<div 
                                       id="cookieConsentNotice-banner" 
-                                      class="cookieConsentNotice__banner cookieConsentNotice__banner__${this.config.bannerStyle} cookieConsentNotice__${this.config.border} cookieConsentNotice__${this.config.position}"
+                                      class="cookieConsentNotice__banner cookieConsentNotice__${this.config.border} cookieConsentNotice__${this.config.position}"
                                       style="background-color: ${this.banner.background};"
                                   >
                                       <h3 style="color: ${this.banner.color};">${this.banner.heading}</h3>
@@ -231,15 +231,14 @@ class CookieConsentNotice {
     }
   }
 
-  start(languaje, obj) {
+  start(language, obj) {
     if (!obj) obj = {}
-    const lang = new LanguagesGC(languaje)
+    const lang = new LanguagesCCN(language)
 
     this.config = {
       border: obj.border || 'border',
       position: obj.position || 'left',
-      hideAfterClick: obj.hideAfterClick || false,
-      bannerStyle: obj.style || 'basic'
+      hideAfterClick: obj.hideAfterClick || false
     }
 
     this.tracking = {
@@ -278,7 +277,7 @@ class CookieConsentNotice {
   }
 }
 
-class LanguagesGC {
+class LanguagesCCN {
   constructor(code) {
     this.init()
     let lang = this.arrLang[code] || this.arrLang['en']
@@ -293,18 +292,18 @@ class LanguagesGC {
   init() {
     this.arrLang = {
       af: {
-        'bannerHeading': 'Ons gebruik koekies',
+        'bannerHeading': 'Hoe ons koekies gebruik',
         'bannerDescription': 'Ons gebruik ons eie koekies en die van derdepartye, om inhoud te verpersoonlik en om webverkeer te ontleed.',
         'bannerLinkText': 'Lees meer oor koekies',
-        'acceptBtnText': 'Aanvaar koekies',
+        'acceptBtnText': 'Aanvaar',
         'rejectBtnText': 'Weier',
         'manageText': 'Koekie-instellings'
       },
       en: {
-        'bannerHeading': 'We use cookies',
+        'bannerHeading': 'How we use cookies',
         'bannerDescription': 'We use our own and third-party cookies to personalize content and to analyze web traffic.',
         'bannerLinkText': 'Read more about cookies',
-        'acceptBtnText': 'Accept cookies',
+        'acceptBtnText': 'Accept',
         'rejectBtnText': 'Reject',
         'manageText': 'Manage cookies'
       }
